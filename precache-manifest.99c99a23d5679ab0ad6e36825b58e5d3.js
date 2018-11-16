403548f3c44c33f6f9ef28adb864eb3ee07d8575
Saman7z <saman.emailed@gmail.com>
2018-11-16 17:00:48 +0330

    Updates

---
+++ precache-manifest.99c99a23d5679ab0ad6e36825b58e5d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "79583ee1fd0424510277",
+    "url": "/Todo/static/js/runtime~main.79583ee1.js"
+  },
+  {
+    "revision": "3d3dd8a746a730469a1e",
+    "url": "/Todo/static/js/main.3d3dd8a7.chunk.js"
+  },
+  {
+    "revision": "a7de431bb5fea5c4ddc1",
+    "url": "/Todo/static/js/1.a7de431b.chunk.js"
+  },
+  {
+    "revision": "3d3dd8a746a730469a1e",
+    "url": "/Todo/static/css/main.21701aa1.chunk.css"
+  }
+];
