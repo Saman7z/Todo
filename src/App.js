4fcf8e0f1be4345606d4150f7d802b9752d83988
Saman7z <saman.emailed@gmail.com>
2018-11-16 16:51:46 +0330

    second commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import TodoEntry from "./components/TodoEntry";
+import TodoItms from "./components/TodoItems";
+import TodoBottom from "./components/TodoBottom";
+import { observer } from "mobx-react";
+
+@observer
+class App extends Component {
+  
+  render() {
+    return (
+      <div id="todoapp" className="todoapp">
+        <TodoEntry />
+        <TodoItms />
+        <TodoBottom />
+      </div>
+    );
+  }
+}
+
+export default App;
